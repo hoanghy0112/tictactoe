86ccf21798e3cfaea69bf454a7e70d2bbbd3fd39
Thi Ca <ntca03@gmail.com>
2021-11-11 22:35:28 +0700

    foo

--- index.cpp
+++ index.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
-int main() {
-   std::cout << "Hello world\n";
+using namespace std;
+
+void printHorizontalLine(int size)
+{
+   for (int i = 0; i < size; i++)
+   {
+      cout << " -------";
+   }
+   cout << endl;
+}
+
+void printVerticalLine(int size)
+{
+   for (int i = 0; i < size + 1; i++)
+   {
+      cout << "|      ";
+   }
+   cout << endl;
+}
+
+void printBoard(int size)
+{
+   for (int i = 0; i < size; i++)
+   {
+      for (int j = 0; j < size; j++)
+      {
+      }
+   }
+}
+
+int main()
+{
+   cout << "Hello world\n";
+
+   cout << "New line\n";
+
    return 0;
 }
