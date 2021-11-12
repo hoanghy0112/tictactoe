95d158179d40b2828fff3704f8e52551c1de925a
Hoang Hy <hoanghy01122003@gmail.com>
2021-11-12 11:52:42 +0700

    build with cmake

---
+++ CMakeFiles/tictactoe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tictactoe.dir/index.cpp.o"
+  "CMakeFiles/tictactoe.dir/index.cpp.o.d"
+  "tictactoe"
+  "tictactoe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tictactoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
